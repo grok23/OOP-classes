cee55c323848a4578634d70b851e807b99f107e7
grok23 <46896171+grok23@users.noreply.github.com>
2019-10-04 19:14:03 +0100

    Q7 part done

---
+++ SchoolsDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//schools data entry will be handled from here
+            List<School> list = new List<School>();
+            list.Add(new School() { Name = "St Patricks", StudentCount = 458 });
+            list.Add(new School() { Name = "Magdalene", StudentCount = 1998 });
+            list.Add(new School() { Name = "The Model", StudentCount = 64 });
+            list.Add(new School() { Name = "Prep", StudentCount = 128 });
+            list.Add(new School() { Name = "Sligo IT", StudentCount = 1024 });
+            list.Add(new School() { Name = "Alpha", StudentCount = 1024 });
+            // Uses IComparable.CompareTo()
+            list.Sort();
+
+            // Uses School.ToString
+            foreach (var school in list)
+            {
+                Console.WriteLine(school);
+            }
+        }
+
+        
+    }
+}
---
+++ SchoolsDemo/School.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolsDemo
+{
+   
+    class School : IComparable<School>
+    {
+        public int StudentCount { get; set; }
+        public string Name { get; set; }
+
+        public int CompareTo(School other)
+        {
+            // Alphabetic sort if StudentCount is equal. [A to Z]
+            if (this.StudentCount == other.StudentCount)
+            {
+                return this.Name.CompareTo(other.Name);
+            }
+            // Default to StudentCount sort. [High to low]
+            return other.StudentCount.CompareTo(this.StudentCount);
+        }
+
+        public override string ToString()
+        {
+            // String representation.
+            return this.StudentCount.ToString() + "," + this.Name;
+        }
+    }
+}
