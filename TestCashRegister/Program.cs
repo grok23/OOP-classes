e7a91d1ae0e1b1f54f2f36d686a81e860f75d61c
grok23 <46896171+grok23@users.noreply.github.com>
2019-10-10 11:25:46 +0100

    Revert "finished up Q7 (for now)"
    This reverts commit 7cdba9fb8ddfcb02a9a34ec825590a47be7174a5.

---
+++ TestCashRegister/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCashRegister
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CashRegister reg1 = new CashRegister();
+            reg1.AddItem(4.99M);
+            reg1.AddItem(9.99M);
+            reg1.AddItem(3.50M);
+            reg1.AddItem(99.98M);
+
+            Console.WriteLine("Basket 1, "+ reg1);
+
+
+            CashRegister reg2 = new CashRegister();
+            reg2.AddItem(10.99M);
+            reg2.AddItem(100.00M);
+            reg2.AddItem(99.80M);
+            reg2.AddItem(0.99M);
+            reg2.AddItem(4.99M);
+
+            Console.WriteLine("Basket 2, "+ reg2);
+            
+
+        }
+    }
+}
